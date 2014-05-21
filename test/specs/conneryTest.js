d9fb2a0ab71d5e7a04da05c4dc2d1ff28f59ed94
Otter <otc@otterthecat.com>
2014-05-20 23:06:11 -0500

    Initial Commit

---
+++ test/specs/conneryTest.js
@@ -0,0 +1,25 @@
+// assertion library
+// /////////////////////////////////////////////////////////
+var chai = require('chai').should();
+
+// stubs
+// /////////////////////////////////////////////////////////
+
+
+// modules to test
+// /////////////////////////////////////////////////////////
+var connery = require('../../lib/connery');
+
+describe('connery', function(){
+
+	describe('#connery()', function(){
+
+		it('should detect Firefox from request', function(){
+			false.should.equal(true);
+		});
+
+		it('should detect OS from request', function(){
+			flase.should.equal(true);
+		});
+	});
+});
